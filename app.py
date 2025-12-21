c1ad5454afe4da14b574bff3d7e8aec3fb557dc3
Bryamx26 <144278205+Bryamx26@users.noreply.github.com>
2025-12-21 02:33:32 +0100

    test

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def argocd():
     <title>ArgoCD - Guide Complet</title>
     <style>
         * {
-            margin: 0;
+            margin: 0; 
             padding: 0;
             box-sizing: border-box;
         }
